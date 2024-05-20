4efe99f462e13e5d8e1eae0b9491c07f78191751
Pako2425 <patryk.kocinski340@gmail.com>
2024-05-20 15:40:39 +0200

    Controller Answering with JSON file after http GET request.

---
+++ backend/src/main/java/com/springreactapp/springreactapp/ExampleAnswer.java
@@ -0,0 +1,13 @@
+package com.springreactapp.springreactapp;
+
+public class ExampleAnswer {
+    private final String message;
+
+    ExampleAnswer() {
+        this.message = "Connection is working!";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- backend/src/main/java/com/springreactapp/springreactapp/ExampleController.java
+++ backend/src/main/java/com/springreactapp/springreactapp/ExampleController.java
@@ -2,13 +2,14 @@ package com.springreactapp.springreactapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ExampleController {
 
-    @GetMapping("/home/example-url/")
-    public String exampleRequest() {
-
-        return "JSON data answer";
+    @GetMapping(value = "/home/example-url")
+    @ResponseBody
+    public ExampleAnswer exampleRequest() {
+        return new ExampleAnswer();
     }
 }
