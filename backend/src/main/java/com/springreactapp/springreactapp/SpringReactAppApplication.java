7e1d6bc794560482c2f8f0ec196ce32692105622
Pako2425 <patryk.kocinski340@gmail.com>
2024-05-21 10:14:30 +0200

    Frontend & backend, problem with communication. No 'CORS' header.

--- backend/src/main/java/com/springreactapp/springreactapp/SpringReactAppApplication.java
+++ backend/src/main/java/com/springreactapp/springreactapp/SpringReactAppApplication.java
@@ -1,6 +1,6 @@
 package com.springreactapp.springreactapp;
 
-import org.springframework.boot.SpringApplication;
+import   org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
